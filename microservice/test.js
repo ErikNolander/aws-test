9dd61ab0e9532dc7a6fc93cd5809197a4c38c5fc
Pandy9 <andypersson2001@gmail>
2026-04-20 08:45:51 +0200

    Correct url and request rate

--- microservice/test.js
+++ microservice/test.js
@@ -2,15 +2,32 @@ import http from 'k6/http';
 import { check } from 'k6';
 
 export const options = {
-  vus: 1,
-  iterations: 1,
+  scenarios: {
+    low_load: {
+      executor: 'constant-arrival-rate',
+      rate: 100,          // 100 requests
+      timeUnit: '1m',     // per minut
+      duration: '1m',
+      preAllocatedVUs: 10,
+      maxVUs: 50,
+    },
+  },
 };
 
 export default function () {
-  const res = http.get('http://13.61.184.112:8080/health');
+  const url = 'http://13.61.184.112:8080/prime-count';
 
-  console.log(`status=${res.status}`);
-  console.log(`body=${res.body}`);
+  const payload = JSON.stringify({
+    limit: 7920,
+  });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const res = http.post(url, payload, params);
 
   check(res, {
     'status is 200': (r) => r.status === 200,
